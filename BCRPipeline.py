3eda9ef9a2ded9f14e44c52c31b7103c87821538
Franz Matthies <franz.matthies@uni-jena.de>
2016-09-20 18:22:40 +0200

    renaming and argument parser for BCR

--- BCRPipeline.py
+++ BCRPipeline.py
@@ -9,7 +9,47 @@ import sys
 import re
 import subprocess
 import threading
+import argparse
 
+
+class PipelineParser(argparse.ArgumentParser):
+    def __init__(self):
+        argparse.ArgumentParser.__init__(self, description="Usenet E-Mail Tokenizer/Lemmatizer")
+        self.add_argument(
+            'input-folder',
+            type = str, nargs = 1, help = 'path to parent email folder'
+        )
+        self.add_argument(
+            '-o', '--out-folder', metavar = 'Output-Folder',
+            action = 'store', nargs = '?', default = None,
+            type = str, help = 'If this argument is given, the produced ".tok" files are stored here; either relative or absolute (default: they are stored alongside their respective original txt files)'
+        )
+        self.add_argument(
+            '-s', '--sent-tok', metavar = 'Sentence Tokenizer',
+            action = 'store', nargs = '?', default = 'german',
+            type = str, help = 'A Tokenizer model (anything that is accesible from NLTK is possible (default: german)'
+        )
+        self.add_argument(
+            '-d', '--word-dict', metavar = 'Word Dictionary',
+            action = 'store', nargs = '?', default = 'pickled_dicts/german/',
+            type = str, help = 'A folder with pickled dictionary items; if this is given, bad characters (e.g. �) are replaced with correct characters according to the BadCharReplacer algorithm that utilizes word unigrams and Edit Distance (default: pickled_dicts/german/)'
+        )
+        self.add_argument(
+            '-r', '--tok-regex', metavar = 'Preliminary Wordtokenizer',
+            action = 'store', nargs = '?', default = '[\w|�]+|[^\w\s]+',
+            type = str, help = 'A regular expression that is used for preliminary word tokenization; these tokens are given to the BadCharReplacer (default: "[\w|�]+|[^\w\s]+" )'
+        )
+        self.add_argument(
+            '-t', '--threads', metavar = 'Number of Threads',
+            action = 'store', nargs = '?', default = 1,
+            type = int, help = 'Number of threads to start (default: 1)'
+        )
+        self.add_argument(
+            '-b', '--bag-of-words', metavar = 'Bag-of-Words file',
+            action = 'store', nargs = '?', default = False,
+            type = bool, help = 'If true, there will also be ".lem" files created that only contains whitespace delimited lemma of an email (default: False)'
+        )
+        
 
 class EmailWalker(object):
     def __init__(self):
@@ -29,7 +69,7 @@ class EmailWalker(object):
         return self.emails
 
 class PipeThread(threading.Thread):
-    def __init__(self, threadID, name, subset, pretokenizer, charrepl, stok, bow=False, out_folder=None):
+    def __init__(self, threadID, name, subset, pretokenizer, charrepl, stok, ifoo, bow=False, out_folder=None):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
@@ -39,6 +79,7 @@ class PipeThread(threading.Thread):
         self.sent_tokenizer = stok
         self.writeBoW = bow
         self.outFolder = out_folder
+        self.main_folder = os.path.abspath(ifoo)
 
     def run(self):
         print("Starting {} with {} E-Mails".format(self.name, len(self.subset)))
@@ -59,24 +100,22 @@ class PipeThread(threading.Thread):
         
     def writeTags(self, tagged, mail):
         out = mail
-        if self.outFolder:
-            ofoo, ofind = self.outFolder
-            if (os.path.dirname(mail)).find(ofind) >= 0:
-                ofoo = os.path.abspath(ofoo)
-                out = os.path.join(ofoo, mail[(os.path.dirname(mail)).find(ofind):])
-            else:
-                out = os.path.join(ofoo, os.path.basename(mail))
+        if self.out_folder:
+            out = os.path.relpath(mail, self.main_folder)
+            os.path.join(self.out_folder, out)
             os.makedirs(os.path.dirname(out), exist_ok=True)
 
         with open(out+".tok", "w") as tfile:
             tfile.write("\n\n".join(tagged))
         if self.writeBoW:
             with open(out+".lem", "w") as lfile:
-                lfile.write(" ".join(
-                    [(line.split("\t")[-1]).lower() for sent in tagged
-                        for line in sent.split("\n") if not re.search("(\$.|\$\()", line.split("\t")[1])]
-                    )
-                )
+                for sent in tagged:
+                    lines = sent.split("\n")
+                    for line in lines:
+                        line = line.split("\t")
+                        if len(line) > 1:
+                            if not re.search("(\$.|\$\()", line[1]):
+                                lfile.write("{} ".format(line[-1]).lower())
 
 def getEmails(base_path):
     base_path = os.path.abspath(base_path)
@@ -129,7 +168,7 @@ def runTreeTagger(tlist, lang="de"):
     
     return tags
 
-def startThreads(fi_list, wpt, bcr, stok, thread_count=4, bow=False, out_folder=None):
+def startThreads(fi_list, wpt, bcr, stok, ifoo, thread_count=4, bow=False, out_folder=None):
     sub_size = int(len(fi_list)/thread_count)
     
     for i in range(thread_count + 1):
@@ -138,28 +177,25 @@ def startThreads(fi_list, wpt, bcr, stok, thread_count=4, bow=False, out_folder=
             subs = fi_list[i*sub_size:]
         
         thread = PipeThread(i, "Subset-{}".format(str(i)),
-            subs, wpt, bcr, stok, bow, out_folder)
+            subs, wpt, bcr, stok, ifoo, bow, out_folder)
         thread.start()
 
 if __name__ == "__main__":
-    
-    main_folder = "/home/matthies/experiments/kodeAlltag/sample/"
-    # ToDo: desc outfolder format
-    out_folder = ("/home/matthies/experiments/kodeAlltag/sample/out/","de.")
-    tok_model = "tokenizers/punkt/german.pickle"
-    wdpath = "pickled_dicts/"
-    regex = "[\w|�]+|[^\w\s]+"
-    thread_count = 4
-    bow = True
+    parser = PipelineParser()
+    args = vars(parser.parse_args())
 
+    mails = getEmails(args['input-folder'][0])
+    wrd_dict = WordDictObject.WordDictObject(args['word_dict'])
+    prel_tok = nltk.tokenize.RegexpTokenizer(args['tok_regex'])
+    sent_tok = nltk.data.load(
+        os.path.join('tokenizers','punkt','{}.pickle'.format(args['sent_tok']))
+    )
+    chr_repl = BadCharReplacer.BadCharReplacer(wrd_dict)
     
-    m = getEmails(main_folder)
-    print(m)
-
     
-    wdo = WordDictObject.WordDictObject(wdpath)
-    wpt = nltk.tokenize.RegexpTokenizer(regex)
-    bcr = BadCharReplacer.BadCharReplacer(wdo)
-    stok = nltk.data.load(tok_model)
-
-    startThreads(m, wpt, bcr, stok, thread_count, bow, out_folder)
+    startThreads(mails, prel_tok, chr_repl, sent_tok,
+        in_folder = args['input-folder'][0],
+        thread_count = args['threads'],
+        bow = args['bag_of_words'],
+        out_folder = args['out_folder']
+    )
